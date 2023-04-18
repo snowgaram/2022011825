bcded447033e92df0bc470710438a590afdf5f40
82107 <82107@DESKTOP-DHG94VU>
2023-04-18 14:10:05 +0900

    init commit

---
+++ helloWorld/src/helloWorld/Dude.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class Dude {
+	public String name;
+	public int hp = 100;
+	public int mp = 0;
+	
+	public void sayName() {
+		System.out.println(name);
+	}
+	public static void main(String[] args) {
+		System.out.println("!!");
+	}
+}
