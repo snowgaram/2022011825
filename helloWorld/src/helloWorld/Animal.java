14e9193c7d70b5c50b010481bcc13a8728c505c5
82107 <82107@DESKTOP-DHG94VU>
2023-04-26 01:44:54 +0900

    init commit

--- helloWorld/src/helloWorld/Animal.java
+++ helloWorld/src/helloWorld/Animal.java
@@ -1,10 +1,22 @@
 package helloWorld;
 
+import java.util.ArrayList;
+
 public class Animal {
 	String name;
 	String crysound;
 	
+	ArrayList<String> animal = new ArrayList();
+	
 	void bark() {
+		Cat cat = new Cat();
+		Cow cow = new Cow();
+		Dog dog = new Dog();
+		Lion lion = new Lion();
 		
+		cat.crysound = "야옹";
+		cow.crysound = "음메";
+		dog.crysound = "멍멍";
+		lion.crysound = "으르렁";
 	}
 }
--- helloWorld/src/helloWorld/Tester.java
+++ helloWorld/src/helloWorld/Tester.java
@@ -1,9 +1,30 @@
 package helloWorld;
 
-public class Tester {
+/*public class Tester {
 	public static void main(String[] args) {
+		Dude dude = new Dude();
 		GrandWizard grandwizard = new GrandWizard();
-		grandwizard.name = "Flash";
+		grandwizard.name = "abc";
+		dude.sayName();
 		grandwizard.sayName();
+// 막내(자식)인 grandwizard 이름에 값 대입. 따라서 부모인 dude와 wizard에는 공백
+	}
+}*/
+
+class Shape{
+	public void printInfo() {
+		System.out.println("shape");
+	}
+}
+class Line extends Shape{
+	public void printInfo() {
+		System.out.println("line");
+	}
+}
+public class Tester{
+	public static void main(String[] args) {
+		Shape s = new Line(); // upcasting
+//		Line q = new Shape(); // donwcasting 
+		s.printInfo();
 	}
 }
